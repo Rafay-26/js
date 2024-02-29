b6536526ac0466c22082fb2cbcf349370b177443
Abdul Rafay <154727770+Rafay-26@users.noreply.github.com>
2024-02-29 11:54:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+document.write("<center><h1>MY CV</h1></center>")
+document.write(" <center><img  border='3px'src='./WhatsApp Image 2024-01-24 at 1.34.51 PM (1).jpeg' width='150px'></center>")
+document.write("<hr>")
+
+var name = "Abdul Rafay"
+var studt = "Class 12"
+var dob = "29/9/2006"
+var email = "abdulrafay12020@gmail.com"
+
+
+document.write("<h1>About:</h1>")
+document.write("<hr>")
+document.write(`<h3>Name: ${name} </h3>`)
+document.write(`<h3> Study: ${studt} </h3>`)
+document.write(`<h3> Date of birth: ${dob} </h3>`)
+document.write(`<h3>Email: ${email} </h3>`)
+document.write("<hr>")
+
+document.write("<h1>Skills:</h1>")
+document.write("<hr>")
+document.write("<ul><h3><li>Html</li><li>Css</li><li>Drawing</li></h3></ul>")
+
+document.write("<hr>")
+document.write(" <center><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbbmKdHtQv62JOgEqJwuIMwwLVYQOuZ_CqWg&usqp=CAU' width='250px'></center>")
+
+
+
+
+
+
+
+
+
+// var num1 = +prompt("Enter number")
+// var num2 = +prompt("Enter another number")
+// var result = num1 + num2
+// var sub = num1 - num2
+// var mult  = num1 * num2
+// var divid = num1 / num2
+
+// document.write(`The addition number  ${num1}  and  ${num2}  is:  ${result}  <br>`)
+// document.write(`The Subtraction  number  ${num1}  and  ${num2}  is:  ${sub}  <br>`)
+// document.write(`The multiply number  ${num1}  and  ${num2}  is:  ${mult}  <br>`)
+// document.write(`The division number  ${num1}  and  ${num2}  is:  ${divid}  <br>`)
+
+
